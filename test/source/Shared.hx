3fc2534e906e5a1bad44daa8944303a63eed10b3
Davvex87 <dvvex87@gmail.com>
2025-05-09 23:31:43 +0100

    evalFile function for classes

--- test/source/Shared.hx
+++ test/source/Shared.hx
@@ -1,3 +1,5 @@
+import haxe.io.Path;
+import sys.io.File;
 import hscript.*;
 
 function evalExpr(expr:String, ?vars: Map<String, Dynamic>, ?params:Array<Dynamic>):Dynamic
@@ -11,12 +13,27 @@ function evalExpr(expr:String, ?vars: Map<String, Dynamic>, ?params:Array<Dynami
 		interp.variables.set("params", params);
 
 	if (vars != null)
-	{
 		for (key => value in vars)
-		{
 			interp.variables.set(key, value);
-		}
-	}
+	
+	return interp.execute(program);
+}
 
+function evalFile(file:String, ?vars:Map<String, Dynamic>):Dynamic
+{
+	var expr = File.getContent(file);
+	var fname = Path.withoutDirectory(file);
+
+	var parser = new SuperParser();
+	var interp = new SuperInterp();
+
+	var program = parser.parseString(expr, fname, 0);
+	var modules = parser.parseModule(expr, fname, 0);
+
+	if (vars != null)
+		for (key => value in vars)
+			interp.variables.set(key, value);
+
+	interp.registerStructures(modules, file);
 	return interp.execute(program);
 }
